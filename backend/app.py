14e4c9c234b6c31cfea96bcac293e44e1a56b2d9
bang <6uequnf@gmail.com>
2026-08-18 10:17:59 +1200

    GitHub setting checking - deleted a comment.

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,6 @@ from flask import Flask
 from flask_cors import CORS
 from extensions import db
 
-# Hi jun! This is Sedric. I'm testing my Git and GitHub skills. Second Try
-
 # Import the Waiter (Routes) we made
 from routes.api_routes import api_bp
 from routes.post_routes import post_bp
